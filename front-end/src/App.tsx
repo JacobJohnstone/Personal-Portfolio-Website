3aa7ba2c389ad187677f4d19b43d87d20b042aa3
Jacob Johnstone <jjohn483@uwo.ca>
2024-08-24 17:06:51 -0400

    Implemented some animations and powerBtn hover

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import Home from "./components/homePage/Home";
 import power from "./images/Power-Btn.png";
 
@@ -7,20 +7,27 @@ import power from "./images/Power-Btn.png";
 function App() {
     const [displayHome, setDisplayHome] = useState<boolean>(false); // For switching to the home page
     const [showPowerBtn, setShowPowerBtn] = useState<boolean>(true); // For initiating powerBtn fade
-    const [disableBtn, setDisableBtn] = useState<boolean>(false);
-    const powerButtonRef = useRef<HTMLButtonElement | null>(null);
+    const [titleMiddle, setTitleMiddle] = useState(true);
+    const [disableBtn, setDisableBtn] = useState<boolean>(false); // Disables button upon click, so can't be clicked while animating
 
     // tailwind static to animation changes
     const powerBtnClasses = `${
         showPowerBtn
-            ? "flex items-center justify-center cursor-pointer bg-btnColour p-2 rounded-full h-9 w-9 sm:h-12 sm:w-12"
-            : "flex items-center justify-center bg-btnColour p-2 rounded-full h-9 w-9 sm:h-12 sm:w-12 animate-powerDisable"
+            ? "flex items-center justify-center cursor-pointer bg-btnColour opacity-30 p-2 rounded-full h-9 w-9 sm:h-12 sm:w-12 hover:opacity-80 transition duration-300 linear"
+            : "flex items-center justify-center bg-btnColour opacity-80 p-2 rounded-full h-9 w-9 sm:h-12 sm:w-12 animate-powerDisable"
+    }`;
+
+    const titleClasses = `${
+        titleMiddle
+            ? "flex flex-row justify-center items-center relative sm:bottom-24 text-4xl sm:text-5xl md:text-8xl lg:text-9xl font-light font-mono z-30"
+            : "flex flex-row justify-center items-center relative text-4xl sm:text-5xl md:text-8xl lg:text-9xl font-light font-mono z-30 animate-phoneTop sm:animate-toTop duration-500"
     }`;
 
     // Called on PowerBtn Click
     function handleClick() {
         setShowPowerBtn(false);
         setDisableBtn(true);
+        setTitleMiddle(false);
         setTimeout(pageTranistion, 5000); // Delay until all animations are complete
     }
 
@@ -41,19 +48,13 @@ function App() {
                         <div className="absolute bg-largeEclipse rounded-full w-screen h-screen blur-3xl sm:blur-[176px] bg-opacity-20"></div>
                         <div className="absolute bg-smallEclipse rounded-full w-1/2 h-3/5 blur-3xl sm:blur-[234px] bg-opacity-30 z-20"></div>
                         <div>
-                            <div
-                                className="flex flex-row justify-center items-center text-transparent relative sm:bottom-24
-                                text-4xl sm:text-5xl md:text-8xl lg:text-9xl font-light font-mono z-30
-                                bg-gradient-to-b from-white to-bottomGradient bg-clip-text">
-                                JACOB JOHNSTONE
-                            </div>
+                            <div className={titleClasses}>JACOB JOHNSTONE</div>
                             <div className="flex m-auto text-white w-fit top-24 sm:top-1 relative z-30">
                                 <div>
                                     <button
                                         onClick={(e) => {
                                             handleClick();
                                         }}
-                                        ref={powerButtonRef}
                                         disabled={disableBtn}>
                                         <img
                                             src={power}
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -9,24 +9,49 @@ module.exports = {
                     "50%": { transform: "9deg" },
                 },
                 disappear: {
-                    from: { opacity: 1 },
+                    from: { opacity: 80 },
                     to: { opacity: 0 },
+                },
+                titleToTop: {
+                    from: {
+                        bottom: "6rem",
+                        color: "white",
+                    },
+                    to: {
+                        bottom: "28rem",
+                        color: "black",
+                    },
+                },
+                phoneTitleToTop: {
+                    from: {
+                        bottom: "0rem",
+                    },
+                    to: {
+                        bottom: "16rem",
+                    },
+                },
+                gradientAnimation: {
+                    from: {
+                        background:
+                            "linear-gradient(to bottom, #FFFFFF, #696969, #FFFFFF)",
+                    },
+                    to: {
+                        background: "#FE01FE",
+                    },
                 },
             },
             colors: {
                 smallEclipse: "#554301",
                 largeEclipse: "#4F2202",
                 bottomGradient: "#696969",
-                btnColour: "#C5C5C5",
-            },
-            blur: {
-                smallBlur: "234px",
-                largeBlur: "176px",
+                btnColour: "#F0EFF0",
             },
             animation: {
                 spinSlow: "spin 60s linear infinite",
-                wiggle: "wiggle 1s linear infinite",
                 powerDisable: "disappear 1s linear forwards",
+                toTop: "titleToTop 2s ease-in-out 1s both",
+                phoneTop: "phoneTitleToTop 2s ease-in-out 1s both",
+                gradient: "gradientAnimation 10s ease 1s forwards",
             },
         },
     },
