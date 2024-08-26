ecf1bc27dc06bb5eb79cc7838ae34e8550241782
Jacob-13 <jjohn483@uwo.ca>
2024-08-26 17:18:47 -0400

    Added title flicker and title background

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -7,7 +7,8 @@ import power from "./images/Power-Btn.png";
 function App() {
     const [displayHome, setDisplayHome] = useState<boolean>(false); // For switching to the home page
     const [showPowerBtn, setShowPowerBtn] = useState<boolean>(true); // For initiating powerBtn fade
-    const [titleMiddle, setTitleMiddle] = useState(true);
+    const [titleMiddle, setTitleMiddle] = useState(true); // For indicating title transition
+    const [titleBackground, setTitleBackground] = useState<boolean>(true); // For
     const [disableBtn, setDisableBtn] = useState<boolean>(false); // Disables button upon click, so can't be clicked while animating
 
     // tailwind static to animation changes
@@ -19,8 +20,14 @@ function App() {
 
     const titleClasses = `${
         titleMiddle
-            ? "flex flex-row justify-center items-center relative sm:bottom-24 text-4xl sm:text-5xl md:text-8xl lg:text-9xl font-light font-mono z-30"
-            : "flex flex-row justify-center items-center relative text-4xl sm:text-5xl md:text-8xl lg:text-9xl font-light font-mono z-30 animate-phoneTop sm:animate-toTop duration-500"
+            ? "flex flex-row justify-center items-center relative text-[#777777] sm:bottom-24 text-4xl sm:text-5xl md:text-8xl lg:text-9xl font-light font-mono z-40"
+            : "flex flex-row justify-center items-center relative text-4xl sm:text-5xl md:text-8xl lg:text-9xl font-light font-mono z-40 animate-phoneTop sm:animate-title"
+    }`;
+
+    const titleBackgroundClasses = `${
+        titleBackground
+            ? "flex flex-row jusitfy-center items-center absolute top-0 left-0 z-30 blur-[50px] text-transparent text-4xl sm:text-5xl md:text-8xl lg:text-9xl font-light font-mono w-fit bg-red-800"
+            : "flex flex-row jusitfy-center items-center absolute z-30 sm:animate-titleBackground"
     }`;
 
     // Called on PowerBtn Click
@@ -28,7 +35,8 @@ function App() {
         setShowPowerBtn(false);
         setDisableBtn(true);
         setTitleMiddle(false);
-        setTimeout(pageTranistion, 5000); // Delay until all animations are complete
+        setTitleBackground(false);
+        setTimeout(pageTranistion, 6000); // Delay until all animations are complete
     }
 
     // Delayed function call for smooth transition
@@ -44,18 +52,22 @@ function App() {
                 </div>
             ) : (
                 <div>
-                    <div className="h-screen flex items-center justify-center text-white bg-gray-800">
+                    <div className="h-screen flex items-center justify-center bg-gray-800">
                         <div className="absolute bg-largeEclipse rounded-full w-screen h-screen blur-3xl sm:blur-[176px] bg-opacity-20"></div>
                         <div className="absolute bg-smallEclipse rounded-full w-1/2 h-3/5 blur-3xl sm:blur-[234px] bg-opacity-30 z-20"></div>
+                        <div className={titleBackgroundClasses}>
+                            JACOB JOHNSTONE
+                        </div>
                         <div>
                             <div className={titleClasses}>JACOB JOHNSTONE</div>
-                            <div className="flex m-auto text-white w-fit top-24 sm:top-1 relative z-30">
+                            <div className="flex m-auto w-fit top-24 sm:top-1 relative z-30">
                                 <div>
                                     <button
                                         onClick={(e) => {
                                             handleClick();
                                         }}
-                                        disabled={disableBtn}>
+                                        disabled={disableBtn}
+                                    >
                                         <img
                                             src={power}
                                             alt="Click-Here"
