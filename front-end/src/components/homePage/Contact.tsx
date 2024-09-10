52c6f410619cc1f345399ee0d05c779db87fbef1
Jacob-13 <jjohn483@uwo.ca>
2024-09-10 15:53:35 -0400

    created social icons

--- front-end/src/components/homePage/Contact.tsx
+++ front-end/src/components/homePage/Contact.tsx
@@ -1,15 +1,41 @@
+import github from "../../images/githubIcon.png";
+import linkedin from "../../images/linkedinIcon.webp";
+import resume from "../../images/resumeIcon.png";
+import email from "../../images/gmailIcon.webp";
+
 const Contact = () => {
     return (
         <div className="w-screen h-fit flex justify-center mt-20 bg-gray-800 z-40 pt-10">
             <div className="flex-block w-2/3 justify-center">
-                <div className="text-titleFinal text-center text-7xl pb-10">
+                <div className="text-titleFinal text-center text-7xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
                     Contact
                 </div>
-                <div className="text-center text-blue-500 text-2xl font-bold">
-                    <div className="pb-2">Resume</div>
-                    <div className="pb-2">LinkedIn</div>
-                    <div className="pb-2">Github</div>
-                    <div className="mb-10">Email</div>
+
+                <div className="w-full flex justify-center">
+                    <div className="flex h-20 w-fit justify-center items-center my-10 bg-gray-300 py-2 rounded-3xl">
+                        <img
+                            src={resume}
+                            className="max-h-full mx-10 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] cursor-pointer"
+                        />
+
+                        <img
+                            src={github}
+                            className="max-h-full mx-10 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] cursor-pointer"
+                        />
+                        <a
+                            href="https://www.linkedin.com/in/jacobjohnstone/"
+                            className="max-h-full"
+                        >
+                            <img
+                                src={linkedin}
+                                className="max-h-full mx-10 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] cursor-pointer"
+                            />
+                        </a>
+                        <img
+                            src={email}
+                            className="max-h-full mx-10 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] cursor-pointer"
+                        />
+                    </div>
                 </div>
             </div>
         </div>
