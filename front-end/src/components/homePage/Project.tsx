1802f7e2fadac187bfb36975903bccbef00c4888
Jacob-13 <jjohn483@uwo.ca>
2024-09-16 17:41:37 -0400

    added music project, fixed technology icons

--- front-end/src/components/homePage/Project.tsx
+++ front-end/src/components/homePage/Project.tsx
@@ -7,6 +7,7 @@ import arrow from "../../images/arrow.png";
 import leftArrow from "../../images/leftArrow.png";
 import heartIcon from "../../images/heartIcon.png";
 import pokeball from "../../images/pokeball-icon.png";
+import musicAppIcon from "../../images/headphoneIcon.webp";
 
 type ProjectProps = {
     title: string;
@@ -37,6 +38,8 @@ const Project = ({
             setImage(heartIcon); // title image
         } else if (title == "Simple Pokedex") {
             setImage(pokeball);
+        } else if (title == "Music Web App") {
+            setImage(musicAppIcon);
         } else {
             setImage(defaultDatabase);
         }
@@ -94,12 +97,12 @@ const Project = ({
                 {description}
             </div>
             <div className="flex-block md:flex mt-4 items-center h-fit text-gray-500">
-                <div className="flex justify-start items-center md:w-1/2 lg:min-h-[25rem]">
+                <div className="flex justify-start items-center md:w-1/2 lg:h-[25rem]">
                     <div className="flex-block relative">
                         <div className="text-lg sm:text-xl md:text-2xl text-titleFinal font-mono pl-3">
                             Features
                         </div>
-                        <ul className="relative mr-2 h-full p-3 overflow-y-scroll no-scrollbar bg-gray-800 rounded-3xl flex-block justify-center w-full">
+                        <ul className="relative mr-2 h-[20rem] p-3 overflow-y-scroll no-scrollbar bg-gray-800 rounded-3xl flex-block justify-center w-full">
                             {features.map((feature, i) => (
                                 <li
                                     key={i}
@@ -113,12 +116,8 @@ const Project = ({
                         <div className="absolute bottom-0 w-full h-10 bg-gradient-to-t from-gray-800 to-transparent z-40 rounded-3xl"></div>
                     </div>
                 </div>
-                <div className="flex justify-center items-center md:w-1/2 sm:mt-5 md:mt-0 md:h-[20rem] lg:h-[25rem] md:ml-5">
-                    <img
-                        src={image}
-                        className="md:h-fit mt-4 md:mt-0 max-h-full rounded-3xl"
-                        alt={alt}
-                    />
+                <div className="flex justify-center items-center md:w-1/2 mt-5 md:mt-0 h-[20rem] md:ml-5 bg-white p-5 rounded-3xl">
+                    <img src={image} className="max-h-full" alt={alt} />
                 </div>
             </div>
             <div className="flex justify-center items-center">
@@ -137,20 +136,20 @@ const Project = ({
                 )}
                 <div
                     ref={iconDivRef}
-                    className="relative flex-block h-fit xs:flex xs:h-16 mt-4 xs:w-fit max-w-full justify-start bg-gradient-to-l from-gray-400 via-gray-100 to-gray-400 rounded-3xl items-center object-contain overflow-x-scroll no-scrollbar"
+                    className="relative w-full flex h-[4rem] xs:h-16 mt-4 xs:w-fit max-w-full justify-start bg-gradient-to-l from-gray-400 via-gray-100 to-gray-400 rounded-3xl items-center object-contain overflow-x-scroll overflow-y-hidden no-scrollbar"
                 >
                     {technologyIcons.map((technology, i) =>
                         technology == express ? (
                             <img
                                 key={i}
                                 src={technology}
-                                className="max-h-full w-fit my-8 px-2 xs:my-0 xs:py-5 xs:px-5"
+                                className="h-[3rem] xs:h-full w-fit my-8 px-2 xs:my-0 py-2 xs:py-5 xs:px-5"
                             />
                         ) : (
                             <img
                                 key={i}
                                 src={technology}
-                                className="max-h-full w-fit my-8 px-2 xs:my-0 xs:py-2 xs:px-5"
+                                className="h-[3rem] xs:h-full w-fit my-8 px-2 xs:my-0 xs:py-2 xs:px-5"
                             />
                         )
                     )}
--- front-end/src/components/homePage/ProjectCollection.tsx
+++ front-end/src/components/homePage/ProjectCollection.tsx
@@ -17,9 +17,21 @@ import git from "../../images/git-icon.png";
 import aws from "../../images/aws-icon.webp";
 import html from "../../images/html-icon.webp";
 import cssIcon from "../../images/css-icon.png";
+import Cuda from "../../images/CUDAIcon.png";
+import insomniaIcon from "../../images/insomnia-icon.png";
+import fireBaseIcon from "../../images/firebaseIcon.webp";
+import MusicLanding from "../../images/MusicLanding.jpg";
+import MusicAdmin from "../../images/Admin.jpg";
+import MusicUser from "../../images/User-home.jpg";
+import musicPwdChange from "../../images/password-change.jpg";
+import musicPlayable from "../../images/Track-playable.jpg";
+import musicPublicPlaylists from "../../images/public-playlists.jpg";
+import musicSignin from "../../images/Sign-in.jpg";
+import musicSearch from "../../images/MusicSearch.gif";
 import modelSetup from "../../images/modelDisplay.jpeg";
 import instructors from "../../images/instructor-levels.png";
 import model from "../../images/modelImage.jpg";
+import modelHeart2 from "../../images/modelHeart2.jpg";
 import pokedex from "../../images/Pokedex.jpg";
 import pokedexBoundary from "../../images/Pokedex-boundary.jpg";
 import pokedexSearch from "../../images/Pokedex-number.jpg";
@@ -57,13 +69,14 @@ const ProjectCollection = () => {
                 <div className="flex-block w-5/6 xs:w-2/3">
                     <Project
                         title="Image Translation for Medical Students"
-                        description="Created an image translation program to be used for medical students in training, enhancing their training by allowing them to view a more realistic version when practicing mitral valve surgery procedures"
+                        description="Created an image translation program to be used for medical students in training, enhancing their practice by creating a more realistic visual for simulated mitral valve surgery procedures. The images displayed only show the artificial heart valve before AI translations."
                         features={[
                             "Translated images taken of a plastic model heart into realistic heart images",
                             "Implemented streamed image translation allowing rapid, continual translation for a camera",
+                            "Displayed results using OpenCV, achieving up to 10 frames per second",
                         ]}
-                        featureImages={[modelSetup, model]}
-                        technologyIcons={[python, pytorch, linux]}
+                        featureImages={[modelSetup, model, modelHeart2]}
+                        technologyIcons={[python, pytorch, git, linux, Cuda]}
                     />
                     <Project
                         title="Relational Database Design and Implementation"
@@ -82,29 +95,43 @@ const ProjectCollection = () => {
                             bootStrap,
                             sqlIcon,
                             restAPI,
+                            git,
                         ]}
                     />
                     {seeAllProjectsBtn && (
                         <div>
                             <div className="relative h-64 xs:h-96 overflow-hidden -top-10">
                                 <Project
-                                    title="Simple Pokedex"
-                                    description="Created a small pokedex using vanilla JavaScript, HTML, and CSS enableing users to search for a pokemon. Search results provide extra information such as rarity."
+                                    title="Music Web App"
+                                    description="A music web application built with an express back-end supported by firebase and a react front-end. Users can sign-up, manage their accounts, and create playlists from the available music data."
                                     features={[
-                                        "Displays the first 20 pokemon in the pokedex with they index and name",
-                                        "Allows users to search for pokemon and displays an alert with the search results",
-                                        "Implemented input validation to ensure name search only contains alphabetical characters and number search is within the boundaries",
+                                        "Has a landing page that includes a welcome message and public playlists created by other users",
+                                        "Includes a public playlist section that displays playlists that have been created by others users and have the public flag",
+                                        "Implemented user authentication and authorization using firebase that ensures input validation for emails and strong password recommendations",
+                                        "Login brings users to their home page where they can create/modify playlists and search for songs",
+                                        "Includes dynamic search results while looking for different tracks",
+                                        "Each search result includes a dropdown option that includes a play button, bringing them to YouTube with search results for the specified track",
+                                        "Users can also logout and are able to change their password if they wish",
+                                        "The admin accounts created to add other admins as well as remove users",
                                     ]}
                                     featureImages={[
-                                        pokedex,
-                                        pokedexSearch,
-                                        pokedexBoundary,
+                                        MusicLanding,
+                                        musicPublicPlaylists,
+                                        musicSignin,
+                                        MusicUser,
+                                        musicSearch,
+                                        musicPlayable,
+                                        musicPwdChange,
+                                        MusicAdmin,
                                     ]}
                                     technologyIcons={[
-                                        html,
-                                        cssIcon,
-                                        javascript,
                                         git,
+                                        fireBaseIcon,
+                                        express,
+                                        reactIcon,
+                                        insomniaIcon,
+                                        nodeJS,
+                                        restAPI,
                                         aws,
                                     ]}
                                 />
@@ -122,9 +149,42 @@ const ProjectCollection = () => {
                             </div>
                         </div>
                     )}
-
                     {seeAllProjects && (
                         <div>
+                            <Project
+                                title="Music Web App"
+                                description="A music web application built with an express back-end supported by firebase and a react front-end. Users can sign-up, manage their accounts, and create playlists from the available music data."
+                                features={[
+                                    "Has a landing page that includes a welcome message and public playlists created by other users",
+                                    "Includes a public playlist section that displays playlists that have been created by others users and have the public flag",
+                                    "Implemented user authentication and authorization using firebase that ensures input validation for emails and strong password recommendations",
+                                    "Login brings users to their home page where they can create/modify playlists and search for songs",
+                                    "Includes dynamic search results while looking for different tracks",
+                                    "Each search result includes a dropdown option that includes a play button, bringing them to YouTube with search results for the specified track",
+                                    "Users can also logout and are able to change their password if they wish",
+                                    "The admin accounts created to add other admins as well as remove users",
+                                ]}
+                                featureImages={[
+                                    MusicLanding,
+                                    musicPublicPlaylists,
+                                    musicSignin,
+                                    MusicUser,
+                                    musicSearch,
+                                    musicPlayable,
+                                    musicPwdChange,
+                                    MusicAdmin,
+                                ]}
+                                technologyIcons={[
+                                    git,
+                                    fireBaseIcon,
+                                    express,
+                                    reactIcon,
+                                    insomniaIcon,
+                                    nodeJS,
+                                    restAPI,
+                                    aws,
+                                ]}
+                            />
                             <Project
                                 title="Simple Pokedex"
                                 description="Created a small pokedex using vanilla JavaScript, HTML, and CSS enableing users to search for a pokemon. Search results provide extra information such as rarity."
